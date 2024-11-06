acf6ec5a695952b88db298a05b0468ed69898147
grafe <maximizhevsk@gmail.com>
2024-11-06 13:28:43 +0400

    дз выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Author siera = new Author("Кэти", "Сьерра");
+        Book learningJava = new Book("Изучаем Java", siera, 2015);
+        learningJava.setYearBook(2020);
+        System.out.println(siera.getFirstName() + " " + siera.getLastName());
+        System.out.println("название: " + learningJava.getNameBook());
+        System.out.println("год издания: " + learningJava.getYearBook());
+        System.out.println();
+
+        Author shildt = new Author("Gerbert", "Shildt");
+        Book fullJava = new Book("Полное руководство", shildt, 2023);
+        fullJava.setYearBook(fullJava.getYearBook() + 1);
+        System.out.println(shildt.getFirstName() + " " + shildt.getLastName());
+        System.out.println("название: " + fullJava.getNameBook());
+        System.out.println("год издания: " + fullJava.getYearBook());
     }
 }
